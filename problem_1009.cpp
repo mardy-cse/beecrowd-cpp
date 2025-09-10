024a33bbe90c3150aa2a93a36d6332f085b2e9d5
John Liton Mardy <125699050+mardy-cse@users.noreply.github.com>
2025-09-10 14:07:27 +0600

    Add files via upload

---
+++ problem_1009.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main(){
+string seller_name;
+double fixed_salary,total_amount_of_sell,monthly_income;
+cin>>seller_name>>fixed_salary>>total_amount_of_sell;
+cout<<fixed<<setprecision(2);
+monthly_income = {fixed_salary+(total_amount_of_sell*15)/100};
+cout<<"TOTAL = R$ "<<monthly_income<<endl;
+
+
+return 0;
+}
